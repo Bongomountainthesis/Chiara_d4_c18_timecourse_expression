c9f1b8636f3d8a6f45301ddfb06e31ae54d77a31
bongomountain <matthew.burney@kcl.ac.uk>
2011-07-20 10:24:57 +0100

    backup

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -3,8 +3,7 @@
 library(beadarray)
 
 
-dataFile = "./Matt\ 4639127051_Sample_Probe_Profile.txt"
-#sampleSheet = "MouseRef-8_V2_0_R0_11278551_A.bgx"
+dataFile = "data/chiara_esc_timecourse_Sample_Probe_Profile.txt"
 
 
 BSData <- readBeadSummaryData(dataFile=dataFile, 
@@ -17,11 +16,12 @@ BSData <- readBeadSummaryData(dataFile=dataFile,
                               
                               )
 
-labels <- read.csv("labels.tsv", sep="\t", as.is=T)
+labels <- read.csv("data/labels.tsv", sep="\t")
 rownames(labels) <- labels$Samples
 
-BSData<-BSData[,1:8]
-labels <- labels[1:8,]
+BSData<-BSData[,c(1:15,17:32)]
+labels <- labels[1:31,]
+check <- sampleNames(BSData)
 sampleNames(BSData)<-labels[sampleNames(BSData),"Label"]
 
 fix.col<-function(x){
@@ -31,20 +31,20 @@ fix.col<-function(x){
 
 exprs(BSData) <- apply(exprs(BSData),2,fix.col) 
 
-save(BSData, file="BSData.RData")
+save(BSData, file="results/BSData.RData")
 
-postscript(file="Boxplotprenorm.ps", horizontal=FALSE)
-boxplot(as.data.frame(log2(exprs(BSData))),  las = 2, outline = FALSE, ylab = "log2(intensity)", col=c(2,2,2,2,2,4,4,4), main="Log2 Expression")
+postscript(file="results/Boxplotprenorm.ps", horizontal=FALSE)
+boxplot(as.data.frame(log2(exprs(BSData))),  las = 2, outline = FALSE, ylab = "log2(intensity)", col=c(2,2,2,2,4,4,4,4,2,2,2,4,4,4,4,2,2,2,2,4,4,4,4,2,2,2,2,4,4,4,4), main="Log2 Expression")
 dev.off()
 
-postscript(file="plotMAXYprenorm.ps", horizontal=FALSE)
-plotMAXY(exprs(BSData), arrays = 1:8,  pch = 16)
+postscript(file="results/plotMAXYprenorm.ps", horizontal=FALSE)
+plotMAXY(exprs(BSData), arrays = 1:31,  pch = 16)
 dev.off()
 
-postscript(file="plotDENSITYpostnorm_threshold.ps", horizontal=FALSE)
+postscript(file="results/plotDENSITYprenorm.ps", horizontal=FALSE)
 E <- log2(exprs(BSData))
 plot(density(E[,1]))
-for(i in 1:8){
+for(i in 1:31){
   lines(density(E[,i]),col=i)
 }
 abline(v=8,col="red",lwd="3",lty="dotted")
@@ -55,23 +55,23 @@ dev.off()
 BSData.quantile = normaliseIllumina(BSData, method = "quantile", transform = "log2")
 
 
-postscript(file="Boxplotpostnorm.ps", horizontal=FALSE)
-boxplot(as.data.frame((exprs(BSData.quantile))),  las = 2, outline = FALSE, ylab = "log2(intensity)", col=c(2,2,2,2,2,4,4,4), main="Log2 Expression")
+postscript(file="results/Boxplotpostnorm.ps", horizontal=FALSE)
+boxplot(as.data.frame((exprs(BSData.quantile))),  las = 2, outline = FALSE, ylab = "log2(intensity)", col=c(2,2,2,2,4,4,4,4,2,2,2,4,4,4,4,2,2,2,2,4,4,4,4,2,2,2,2,4,4,4,4), main="Log2 Expression")
 dev.off()
 
-postscript(file="plotMAXYpostnorm.ps", horizontal=FALSE)
-plotMAXY(exprs(BSData.quantile), arrays = 1:8, log = FALSE, pch = 16)
+postscript(file="results/plotMAXYpostnorm.ps", horizontal=FALSE)
+plotMAXY(exprs(BSData.quantile), arrays = 1:31, log = FALSE, pch = 16)
 dev.off()
 
-save(BSData.quantile, file="BSData.quantile.RData")
+save(BSData.quantile, file="results/BSData.quantile.RData")
 
-postscript(file="plotDENSITYpostnorm_threshold.ps", horizontal=FALSE)
+postscript(file="results/plotDENSITYpostnorm.ps", horizontal=FALSE)
 E <- exprs(BSData.quantile)
 plot(density(E[,1]),
 	main="",
 	xlab="Raw expression level"
 	)
-for(i in 1:8){
+for(i in 1:31){
   lines(density(E[,i]),col=i)
 }
 abline(v=8,lty="dotted",lwd="3",col="red")
--- scripts/microarrayanalysis.R
+++ scripts/microarrayanalysis.R
@@ -1,20 +1,35 @@
 #!/usr/local/bin/Rscript
 
+stringsAsFactors=FALSE
+
 library(beadarray)
 library(limma)
 library(biomaRt)
 library(illuminaMousev2BeadID.db)
+library(illuminaMousev2.db)
 
 		#load the data
 BSData <- get(load("results/BSData.quantile.RData"))
 E <- exprs(BSData)
 
-design<-matrix(0,nrow=(ncol(E)), ncol=2)
-colnames(design) <- c("MLA_NS","MLA_Neurons")
+E <- E[,c(1:4,24:27,28:31,5:8,9:11,16:19,20:23,12:15)]
+
+design<-matrix(0,nrow=(ncol(E)), ncol=8)
+colnames(design) <- c("D4_0","D4_1","D4_2","D4_4","C18_0","C18_1","C18_2","C18_4")
 rownames(design) <- colnames(E)
-design[1:5,1] <- 1
-design[6:8,2] <- 1
-cont.matrix<-makeContrasts(NSvNeuron=MLA_Neurons-MLA_NS, levels=design)
+design[1:4,1] <- 1
+design[5:8,2] <- 1
+design[9:12,3] <- 1
+design[13:16,4] <- 1
+design[17:19,5] <- 1
+design[20:23,6] <- 1
+design[24:27,7] <- 1
+design[28:31,8] <- 1
+cont.matrix<-makeContrasts(day0=C18_0-D4_0,
+			   day1=C18_1-D4_1,
+                           day2=C18_2-D4_2,
+                           day4=C18_4-D4_4,
+                           levels=design)
 
 
 fit<-lmFit(E, design)
@@ -58,13 +73,120 @@ rownames(anno) <- anno[,"ID"]
 
 ebFit$genes = anno 
 
-write.fit(ebFit, file="limma_ebfit.csv", adjust="BH")
-data<-read.table("limma_ebfit.csv", sep="\t", header=T)
+f.test<- topTable(ebFit, number=nrow(E))
+rownames(f.test)<-f.test$ID
+#f.test<-f.test[which(f.test[,"adj.P.Val"]<=0.005),]
+f.test<-f.test[order(f.test[,"logFC"],decreasing=TRUE),]
+write.csv(f.test,"results/f_test.csv",row.names=F)
+
+day0<-topTable(ebFit, coef=1, adjust="BH", number=nrow(E))
+rownames(day0)<-day0$ID
+#day0<-day0[which(day0[,"adj.P.Val"]<=0.005),]
+day0<-day0[order(day0[,"logFC"],decreasing=TRUE),]
+write.csv(day0,"results/day0.csv",row.names=F)
+
+day1<-topTable(ebFit, coef=2, adjust="BH", number=nrow(E))
+rownames(day1)<-day1$ID
+#day1<-day1[which(day1[,"adj.P.Val"]<=0.005),]
+day1<-day1[order(day1[,"logFC"],decreasing=TRUE),]
+write.csv(day1,"results/day1.csv",row.names=F)
+
+day2<-topTable(ebFit, coef=3, adjust="BH", number=nrow(E))
+rownames(day2)<-day2$ID
+#day2<-day2[which(day2[,"adj.P.Val"]<=0.005),]
+day2<-day2[order(day2[,"logFC"],decreasing=TRUE),]
+write.csv(day2,"results/day2.csv",row.names=F)
+
+day4<-topTable(ebFit, coef=4, adjust="BH", number=nrow(E))
+rownames(day4)<-day4$ID
+#day4<-day4[which(day4[,"adj.P.Val"]<=0.005),]
+day4<-day4[order(day4[,"logFC"],decreasing=TRUE),]
+write.csv(day4,"results/day4.csv",row.names=F)
 
 
-new.data<- topTable(ebFit, number=nrow(E))
-rownames(new.data)<-new.data$ID
-new.data<-new.data[order(new.data[,"P.Value"]),]
-write.csv(new.data,"limma_results.csv",row.names=F)
+############do something shiny....
+
+#######Make a heatmap
+##calculate averages
+
+D4.0 <- c(1,2,3,4)
+D4.1 <- c(5,6,7,8)
+D4.2 <- c(9,10,11,12)
+D4.4 <- c(13,14,15,16)
+C18.0 <- c(17,18,19)
+C18.1 <- c(20,21,22,23)
+C18.2 <- c(24,25,26,27)
+C18.4 <- c(28,29,30,31)
+
+aves <- apply(E, 1, function(x){
+        D4.0av <- sum(x[D4.0])/length(D4.0)
+        D4.1av <- sum(x[D4.1])/length(D4.1)
+        D4.2av <- sum(x[D4.2])/length(D4.2)
+        D4.4av <- sum(x[D4.4])/length(D4.4)
+        C18.0av <- sum(x[C18.0])/length(C18.0)
+        C18.1av <- sum(x[C18.1])/length(C18.1)
+        C18.2av <- sum(x[C18.2])/length(C18.2)
+        C18.4av <- sum(x[C18.4])/length(C18.4) 
+            return(c(D4.0av,D4.1av,D4.2av,D4.4av,C18.0av,C18.1av,C18.2av,C18.4av))
+        }
+)        
+
+aves <- t(aves)
+
+colnames(aves) <- c("D4_0","D4_1","D4_2","D4_4","C18_0","C18_1","C18_2","C18_4")
+
+#reorder by rowname (probe ID) and then add symbol column
+
+aves.reorder <- aves[order(rownames(aves),decreasing=FALSE),]
+
+#remove duplicate probes, leave the highest FC
+
+f.test.ord <- f.test[order(f.test[,"adj.P.Val"],decreasing=FALSE),]
+
+f.test.dup <- f.test.ord[!duplicated(f.test.ord[,"symbol"]),]
+
+#get same probe ID to remove from averaged E
+f.test.dup.ID <- f.test.dup[,"ID"]
+aves.reorder.dup <- aves.reorder[which(rownames(aves.reorder) %in% f.test.dup.ID),]
+
+#get names from reordered f.test
+
+symbol <- f.test.dup[order(f.test.dup[,"ID"],decreasing=FALSE),"symbol"]
+
+rownames(aves.reorder.dup) <- symbol
+
+#draw heatmap
+
+library(gplots)
+
+#filter for genes that are expressed somewhere
+#test <- apply(aves.reorder.dup,1,function(x){any(x>9)})
+testnames <- g
+filteredE<-aves.reorder.dup[test,]
+
+postscript(file="results/heatmap.ps", horizontal=FALSE)
+heatmap.2(filteredE[,5:8],
+		Rowv=TRUE,
+		Colv=NA,
+		col=greenred(75), 
+		scale="none",
+		key=TRUE,
+		keysize=0.75,
+		symkey=FALSE,
+		density.info="none",
+		trace="none", 
+		labRow=NA,
+		labCol=NA,
+		cexRow=0.75,
+	)
+dev.off()
+
+
+
+
+
+
+
+
 
 
